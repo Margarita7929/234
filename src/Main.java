3c7eecb2d88860b0063dbb7e041a56fe59f4f820
Margarita7929 <bykovskayamargarita@gmail.com>
2022-01-04 01:10:03 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketCost = 200;
+        int sumForMile = 20;
+        int numberOfMiles = ticketCost / sumForMile;
+        System.out.println(numberOfMiles + " бонусных миль");
+    }
+}
